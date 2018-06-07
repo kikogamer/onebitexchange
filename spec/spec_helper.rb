ba0d50860fbc02ffdc38507376acb1e04b05588b
Ronaldo <kikogamer@gmail.com>
2018-06-07 00:25:46 -0300

    Exchange Service

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,15 +1,7 @@
-require 'spec_helper'
-require 'json'
-require './app/services/exchange_service'
-
-describe 'Currency' do
-  it 'exchange' do
-    amount = rand(0..9999)
-    res = ExchangeService.new("USD", "BRL", amount).perform
-    expect(res.is_a? Numeric).to eql(true)
-    expect(res != 0 || amount == 0).to eql(true)
-  end
-end
+require 'webmock/rspec'
+WebMock.disable_net_connect!(allow_localhost: true)
+require File.expand_path("../../config/environment", __FILE__)
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
