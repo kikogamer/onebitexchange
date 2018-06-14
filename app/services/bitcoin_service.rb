ce65a40778e2a2b4a1b7e5d0303ecea4f0bcaeb8
Ronaldo <kikogamer@gmail.com>
2018-06-14 00:22:00 -0300

    Bitcoin Service

---
+++ app/services/bitcoin_service.rb
@@ -0,0 +1,29 @@
+require 'rest-client'
+require 'json'
+
+class BitcoinService
+  def initialize(currency, amount, convertTo)
+    @currency = currency
+    @convertTo = convertTo
+    @amount = amount.to_f
+  end
+
+  def perform
+    begin
+      bitcoin_api_url = Rails.application.credentials[Rails.env.to_sym][:bitcoin_api_url]
+      url = "#{bitcoin_api_url}?currency=#{@currency}&value=#{@amount}"
+      res = RestClient.get url
+      value = res.body.to_f
+
+      if value == 0
+        0
+      elsif @convertTo
+        @amount / value
+      else
+        @amount * value
+      end
+    rescue RestClient::ExceptionWithResponse => e
+      e.response
+    end
+  end
+end
--- spec/services/bitcoin_service_spec.rb
+++ spec/services/bitcoin_service_spec.rb
@@ -5,7 +5,7 @@ require './app/services/bitcoin_service'
 describe 'Currency' do
   it 'bitcoin' do
     amount = rand(0..9999)
-    res = BitcoinService.new("USD", amount).perform
+    res = BitcoinService.new("USD", amount, true).perform
     expect(res.is_a? Numeric).to eql(true)
     expect(res != 0 || amount == 0).to eql(true)
   end
